5e888871e2c6b1da643fb4809b8cb8061eb8590b
佐藤瑞基 <sato.mizuki@sysystem.co.jp>
2025-09-11 08:54:53 +0000

    どれが小さくなってるの？

--- src/components/tabs/dashboard-tab.tsx
+++ src/components/tabs/dashboard-tab.tsx
@@ -296,7 +296,7 @@ export function DashboardTab() {
                       <span>編集</span>
                     </Button>
                   </DialogTrigger>
-                  <DialogContent className="sm:max-w-sm">
+                  <DialogContent>
                     <DialogHeader>
                       <DialogTitle>個人目標の編集</DialogTitle>
                       <DialogDescription>
