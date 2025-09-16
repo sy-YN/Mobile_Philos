1cfbabe21494bcbb0b9e4c3a6989db2bd979ca36
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-09-16 08:15:40 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
+  },
   images: {
     remotePatterns: [
       {
