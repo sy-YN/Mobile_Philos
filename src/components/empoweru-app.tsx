e7f4a6f3d3168ca7637106aa2132752584477790
佐藤瑞基 <sato.mizuki@sysystem.co.jp>
2025-09-18 08:04:15 +0000

    経営層のメッセージと同じように横幅を狭めてほしい

--- src/components/empoweru-app.tsx
+++ src/components/empoweru-app.tsx
@@ -72,7 +72,7 @@ export default function EmpowerUApp() {
       />
 
       <Dialog open={!!selectedNotification} onOpenChange={(isOpen) => !isOpen && setSelectedNotification(null)}>
-        <DialogContent className="max-w-sm">
+        <DialogContent className="max-w-xs">
           {selectedNotification && (
             <>
               <DialogHeader>
