8e1d0fcddbc6934e63e2721e0ebdab832ae0c7df
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-09-29 08:00:45 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/board-post-card.tsx
+++ src/components/board-post-card.tsx
@@ -29,6 +29,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { BoardReplyCard } from "./board-reply-card";
 
--- src/components/board-reply-card.tsx
+++ src/components/board-reply-card.tsx
@@ -27,6 +27,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
 type BoardReplyCardProps = {
